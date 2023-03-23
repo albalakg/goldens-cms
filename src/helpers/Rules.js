f6a9da7da8400184b04ca0ddd23e6c34bd416011
Blacky <gal.blacky@gmail.com>
2023-03-23 19:49:50 +0200

    fix(eslint errors)

--- src/helpers/Rules.js
+++ src/helpers/Rules.js
@@ -1,8 +1,8 @@
 export const FIRST_NAME_RULE            = /.{2,30}/;
 export const LAST_NAME_RULE             = /.{2,30}/;
 export const EMAIL_RULE                 = /^.+@.+\..+$/;
-export const PHONE_RULE                 = /^[\d+\-]{7,13}$/;
-export const PASSWORD_RULE              = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d#$@!%&*\^?]{8,40}$/;
+export const PHONE_RULE                 = /^[\d+-]{7,13}$/;
+export const PASSWORD_RULE              = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d#$@!%&*^?]{8,40}$/;
 export const VIDEO_DESCRIPTION_RULE     = /.{0,1000}/;
 export const SKILL_DESCRIPTION_RULE     = /.{0,1000}/;
 export const TERM_DESCRIPTION_RULE      = /.{0,1000}/;
@@ -16,7 +16,9 @@ export const VIDEO_FILE_TYPES_RULE      = ['video/mp4'];
 export const IMAGE_FILE_TYPES_RULE      = ['image/webp', 'image/png', 'image/jpeg'];
 export const PRICE_RULE                 = /^\d{1,7}\.?\d{0,2}$/;
 export const COUPON_TYPE_RULE           = /^[12]$/;
+// eslint-disable-next-line no-useless-escape
 export const DISCOUNT_RULE              = /^[\d\.]{1,4}$/;
+// eslint-disable-next-line no-useless-escape
 export const COURSE_DISCOUNT_RULE       = /[\d\.]{0,4}/;
 export const NAME_RULE                  = /.{2,40}/;
 export const TRAINING_OPTION_NAME_RULE  = /.{2,100}/;
